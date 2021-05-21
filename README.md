7d53910d979e6a1eb892107909088fdecac0e4c6
sbx_user1051 <sbx_user1051@169.254.14.97>
2021-05-21 17:07:13 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Your submission
+
+This is the README for your submission, add any relevant information to it.
+
+You can find details instruction in the INSTRUCTIONS.md file.
+
+## Submitting your solution
+
+Please push your changes to the `master branch` of this repository. You can push one or more commits. <br>
+
+Once you are finished with the task, please click the `Complete task` link on <a href="https://app.codescreen.dev/#/codescreentest459c8730-f9fd-46a2-b4e4-7c8f6dc95297" target="_blank">this screen</a>.
---
+++ src/test/java/com/marshmallow/hiring/ControllerTest.java
@@ -0,0 +1,43 @@
+package com.marshmallow.hiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ControllerTest {
+
+    // ⚠️ Warning ⚠️
+    // This test is here to ensure you have exposed an endpoint on the right path for our automated tests running once you submit.
+    // Please do not change this file. If you do change it, Codescreen will prevent you from submitting your solution.
+    // You can add your own tests in new files, only this file is protected by Codescreen.
+
+    private static final MockHttpServletRequestBuilder REQUEST_BUILDER =
+        request(HttpMethod.POST, "/instructions")
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .accept(APPLICATION_JSON);
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void ensuresCorrectEndpointIsExposed() throws Exception {
+        mockMvc.perform(REQUEST_BUILDER.content(BASIC_CASE_REQUEST))
+            .andExpect(status().isOk());
+    }
+
+    private static final String BASIC_CASE_REQUEST =
+        "{\"areaSize\": [5, 5], \"startingPosition\": [1, 2], \"oilPatches\": [[1, 0], [2, 2], [2, 3]], \"navigationInstructions\": \"NNESEESWNWW\"}";
+
+}
